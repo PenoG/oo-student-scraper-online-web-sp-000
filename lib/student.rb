dcbbd23d55cc7b3237e379d90b2eec6e6ab7b499
Mason Phillips <mjphillips2000@gmail.com>
2020-11-23 19:33:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |name, location|
+      self.send("#{name}=", location)
+    end
+    self
   end
 
   def add_student_attributes(attributes_hash)
